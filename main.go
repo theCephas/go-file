a9814a13adf55ed7d5202a4df143b0ee8fd4532c
theCephas <oshoiseoluwa@gmail.com>
2024-01-04 23:21:40 +0100

    updates on things I have learnt today

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main(){
 
-	var ages [3]int = [3]int{1, 2, 3}
-	
-	fmt.Println(ages, len(ages))
+	greeting := "Hello, world!"
 
-	//  Slices
-	var scores = []int{1, 2, 3, 4, 5}
-	scores = append(scores, 8)
+	fmt.Println(strings.Index(greeting, "l"))
 
-	fmt.Println(scores, len(scores))
-
-	// Slice ranges
-	words := [6]string{"bells", "shoes", "fan", "cloth", "legs"}
-	rangeOne := words[3:5]
-	// Result is [cloth legs]
-	rangeOne = words[2:5]
-	// Result is [fan legs]
-	
-	fmt.Println(rangeOne)
 }
